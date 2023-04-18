747b0b5f9f8e34fcdc7629fbd9c7f468946eeeee
Lina Budkova <linabudkova35@gmail.com>
2023-04-18 15:47:36 +0300

    up key

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,7 +5,8 @@ const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 const options = {
   params: { maxResults: "50" },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    // "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": "9788bd998cmsh2c270e64909c9eep103cefjsn73fed5bc69a7",
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };

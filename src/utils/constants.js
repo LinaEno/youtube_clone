916bda7c55a9bfa74664b251ec862cd9f5aa3fe5
Lina Budkova <linabudkova35@gmail.com>
2023-04-23 12:11:02 +0300

    up

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,16 @@
-import FiberNewIcon from '@mui/icons-material/FiberNew';
+import FiberNewIcon from "@mui/icons-material/FiberNew";
 import LiveTvIcon from "@mui/icons-material/LiveTv";
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
-import MovieFilterIcon from '@mui/icons-material/MovieFilter';
-import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
-import NewspaperIcon from '@mui/icons-material/Newspaper';
-import SchoolIcon from '@mui/icons-material/School';
-import PodcastsIcon from '@mui/icons-material/Podcasts';
-import SportsIcon from '@mui/icons-material/Sports';
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import MovieFilterIcon from "@mui/icons-material/MovieFilter";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import SchoolIcon from "@mui/icons-material/School";
+import PodcastsIcon from "@mui/icons-material/Podcasts";
+import SportsIcon from "@mui/icons-material/Sports";
 import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
-import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
-import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin";
 
 export const categories = [
   { name: "New", icon: <FiberNewIcon /> },
